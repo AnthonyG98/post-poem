400f73baec1c32d32408e5bf5146f1939c459620
AnthonyG98 <anthony.g6@gmail.com>
2022-05-16 22:00:07 -0700

    cors

--- index.js
+++ index.js
@@ -10,15 +10,8 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
-// app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors(corsOptions));
+
 const db = require("./models");
 
 //Routes
