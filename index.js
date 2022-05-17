24d4f3452b3a03f07ca20ffb4eb411a866ff5257
AnthonyG98 <anthony.g6@gmail.com>
2022-05-16 22:26:33 -0700

    a

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 app.use(express.json());
 const corsOptions = {
-  origin: "https://postpoem.netlify.app",
+  origin: "https://post-poem.herokuapp.com",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
