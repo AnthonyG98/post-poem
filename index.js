ba2dfd3dbfe5f961db680ba2ff21924f75867fab
AnthonyG98 <anthony.g6@gmail.com>
2022-05-17 18:31:52 -0700

    cors

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.use(cors());
