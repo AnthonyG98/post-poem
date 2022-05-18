f9359398dd85d83a1390874707f00b3ef01e7f49
AnthonyG98 <anthony.g6@gmail.com>
2022-05-17 18:08:01 -0700

    a

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 
 app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.removeHeader("x-powered-by");
-  //set the allowed HTTP methods to be requested
-  //   res.header("Access-Control-Allow-Methods", "POST");
-  //headers clients can use in their requests
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //allow request to continue and be handled by routes
   next();
 });
 app.use(cors());
