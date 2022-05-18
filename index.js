d265d2120ac584cda0b2fb72d745d2283d8f4f8c
AnthonyG98 <anthony.g6@gmail.com>
2022-05-17 19:01:04 -0700

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
+app.use(express.json());
 app.use(cors());
 
 const db = require("./models");
